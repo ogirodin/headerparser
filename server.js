e4bcba419a0d79a0852afa40629be708540e1d04
ogirodin <ogirodin@hotmail.com>
2022-02-01 10:40:33 +0000

    adding /api/whoami route for headers parsing

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// your first API endpoint... 
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.headers.host.split('.').shift(),
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"],
+  });
+});
+
 
 
 // listen for requests :)
